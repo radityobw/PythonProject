1a37dd2936f38033e3f8f74a0452608dd9cfa298
ryocantsleep <ryocantsleep@gmail.com>
2025-05-14 14:34:06 +0700

    Menambahkan fitur obesitas dan merapihkan tampilan ui

--- Kalkulator BMI/kalkulator-bmi.py
+++ Kalkulator BMI/kalkulator-bmi.py
@@ -11,19 +11,25 @@ bbideal = dict()
 bbideal['bawah'] = 18.5*(tb**2)
 bbideal['atas'] = 25*(tb**2)
 
-print("--------------------------------")
+
 if bmi < 18.5:
-    print("Hasil: Berat badan Anda kurang ideal, naikkan berat badan Anda!")
+    kategori = "Berat badan Anda kurang ideal, naikkan berat badan Anda!"
 elif bmi >= 18.5 and bmi <= 25:
-    print("Hasil: Berat badan Anda ideal!")
+    kategori = "Berat badan Anda ideal!"
+elif bmi >= 25 and bmi <= 30:
+    kategori = "Berat badan Anda kurang ideal, kurangi berat badan Anda!"
 else:
-    print("Hasil: Berat badan Anda kurang ideal, kurangi berat badan Anda!")
+    kategori = "Anda sangat kelebihan berat badan (Obesitas), mohon kurangi berat badan Anda!"
+
+print("\nHasil kalkulator BMI Anda adalah:")
+print("--------------------------------")
+print(kategori)
 
 print(f"Berat badan ideal Anda adalah dalam rentang " f"{bbideal['bawah']:.2f} kg - {bbideal['atas']:.2f} kg")
 
 print("--------------------------------")
 print(f"BODY MASS INDEKS ANDA: {bmi:.2f} kg/m^2")
-print("Note: BMI normal adalah 18.5 kg/m^2 - 25 kg/m^2")
+print("Note: BMI normal adalah dalam rentang 18.5 kg/m^2 - 25 kg/m^2")
 
 print("--------------------------------")
 print("Terima kasih sudah menggunakan program ini :)")
