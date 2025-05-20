651dcc92732cef0a6ed73d64dd64e205c0eea86b
ryocantsleep <ryocantsleep@gmail.com>
2025-05-20 15:25:37 +0700

    merapihkan banyak hal dengan fitur fungsi def dalam main.py (manajemen kontak)

--- manajemen-kontak/main.py
+++ manajemen-kontak/main.py
@@ -1,8 +1,62 @@
-import email
+# MANAJEMEN KONTAK
 
-kontak1 = {'nama': 'Radit', 'no HP': '090909090909', 'email': 'nibbler@gmail.com'}
-kontak2 = {'nama': 'Tya', 'no HP': '080808080808', 'email': 'cupcake@gmail.com'}
-kontak = [kontak1, kontak2]
+def melihat_kontak():
+    # melihat semua kontak
+    if kontak:
+        for num, item in enumerate(kontak, start=1):
+            print(f"\n{num}. {item['nama']} ({item['no HP']}, {item['email']})")
+    else:
+        print("\nKontak kosong!")
+
+def menambah_kontak():
+    while True:  # sub-loop untuk penambahan kontak
+        nama = input('Masukkan nama kontak yang baru: ')
+        no_HP = input('Masukkan no HP kontak yang baru: ')
+        email = input('Masukkan email kontak yang baru: ')
+        konfirmasi = input('Apakah informasi sudah benar? (ya/tidak/kembali): ')
+
+        if konfirmasi == 'ya':
+            kontakBaru = {'nama': nama, 'no HP': no_HP, 'email': email}
+            kontak.append(kontakBaru)
+            print("\nKontak baru berhasil ditambahkan!")
+            break  # keluar dari sub-loop setelah berhasil
+        elif konfirmasi == 'tidak':
+            print("\nSilakan masukkan ulang data kontak.")
+            continue  # ulangi sub-loop
+        elif konfirmasi == 'kembali':
+            break
+        else:
+            print("\nInput salah! Silakan pilih 'ya', 'tidak' atau 'kembali'.")
+            continue
+
+def menghapus_kontak():
+    while True:  # sub-loop untuk penghapusan kontak
+        melihat_kontak()
+
+        try:
+            iHapus = int(input('\nMasukkan angka kontak yang akan dihapus: '))
+            if iHapus < 1 or iHapus > len(kontak):
+                print("Nomor kontak tidak valid!")
+                continue
+        except ValueError:
+            print("Input harus berupa angka!")
+            continue
+
+        konfirmasi = input('Apakah Anda yakin ingin menghapus kontak? (ya/tidak/kembali): ')
+        if konfirmasi == 'ya':
+            del kontak[iHapus - 1]
+            print('\nKontak berhasil dihapus!')
+            break  # keluar dari sub-loop
+        elif konfirmasi == 'tidak':
+            print("\nPenghapusan dibatalkan. Silakan pilih ulang.")
+            continue  # ulangi sub-loop
+        elif konfirmasi == 'kembali':
+            break
+        else:
+            print("\nInput salah! Silakan pilih 'ya', 'tidak' atau 'kembali'.")
+            continue
+
+kontak = []
 
 while True:
     print('\n------------------------')
@@ -17,76 +71,16 @@ while True:
     pilihan = input("Masukkan pilihan menu kontak (1,2,3,4): ")
 
     if pilihan == '1':
-        # melihat semua kontak
-        if kontak:
-            for num, item in enumerate(kontak, start=1):
-                print(f"\n{num}. {item['nama']} ({item['no HP']}, {item['email']})")
-        else:
-            print("\nKontak kosong!")
+        melihat_kontak()
 
     elif pilihan == '2':
-
-        while True:  # sub-loop untuk penambahan kontak
-            nama = input('Masukkan nama kontak yang baru: ')
-            no_HP = input('Masukkan no HP kontak yang baru: ')
-            email = input('Masukkan email kontak yang baru: ')
-
-            konfirmasi = input('Apakah informasi sudah benar? (ya/tidak/kembali): ')
-
-            if konfirmasi == 'ya':
-                kontakBaru = {'nama': nama, 'no HP': no_HP, 'email': email}
-                kontak.append(kontakBaru)
-                print("\nKontak baru berhasil ditambahkan!")
-                break  # keluar dari sub-loop setelah berhasil
-
-            elif konfirmasi == 'tidak':
-                print("\nSilakan masukkan ulang data kontak.")
-                continue  # ulangi sub-loop
-
-            elif konfirmasi == 'kembali':
-                break
-
-            else:
-                print("\nInput salah! Silakan pilih 'ya' atau 'tidak'.")
+        menambah_kontak()
 
     elif pilihan == '3':
-
         if not kontak:
             print("\nKontak kosong!")
             continue
-
-        while True:  # sub-loop untuk penghapusan kontak
-
-            for num, item in enumerate(kontak, start=1):
-                print(f"\n{num}. {item['nama']} ({item['no HP']}, {item['email']})")
-
-            try:
-                iHapus = int(input('\nMasukkan angka kontak yang akan dihapus: '))
-
-                if iHapus < 1 or iHapus > len(kontak):
-                    print("Nomor kontak tidak valid!")
-                    continue
-
-            except ValueError:
-                print("Input harus berupa angka!")
-                continue
-
-            konfirmasi = input('Apakah Anda yakin ingin menghapus kontak? (ya/tidak/kembali): ')
-
-            if konfirmasi == 'ya':
-                del kontak[iHapus - 1]
-                print('\nKontak berhasil dihapus!')
-                break  # keluar dari sub-loop
-
-            elif konfirmasi == 'tidak':
-                print("\nPenghapusan dibatalkan. Silakan pilih ulang.")
-                continue  # ulangi sub-loop
-
-            elif konfirmasi == 'kembali':
-                break
-
-            else:
-                print("\nInput salah! Silakan pilih 'ya' atau 'tidak'.")
+        menghapus_kontak()
 
     elif pilihan == '4':
         # keluar dari kontak
